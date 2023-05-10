43ca4cca801f194081b341c11f45829a70e5a36e
valeriagmza <133097094+valeriagmza@users.noreply.github.com>
2023-05-10 10:30:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
  
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) 
 streamlit.dataframe(my_fruit_list)
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 
